efc4ce948b2ff05de32c6580c14df87f58adf863
Alex <github@larus.se>
2021-06-21 13:35:45 +0200

    Moves script to sep. file

---
+++ www/log.js
@@ -0,0 +1,16 @@
+"use strict";
+function postJSON(url, data) {
+        var xhr = new XMLHttpRequest();
+        xhr.open("POST", url, true);
+        xhr.setRequestHeader("Content-Type", "application/json");
+        xhr.send(JSON.stringify(data));
+}
+var data = {
+        ts: Math.round(Date.now() / 1000),
+        tz: Intl.DateTimeFormat().resolvedOptions().timeZone,
+        ua: window.navigator.userAgent,
+        ref: document.referrer,
+        url: [location.protocol, '//', location.host, location.pathname].join(''),
+};
+console.log(JSON.stringify(data));
+postJSON("https://c56gd3.deta.dev/post", data);
